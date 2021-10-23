a9f4e6ef7acc4077e8635c1ecf95c37f085e245d
shengzhaoli.shengz <shengzhaoli.shengz@alibaba-inc.com>
2021-10-23 12:23:07 +0800

    Update spring-boot 2.4.2 , update config

--- src/main/java/com/monkeyk/sos/config/JWTTokenStoreConfiguration.java
+++ src/main/java/com/monkeyk/sos/config/JWTTokenStoreConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
@@ -70,6 +71,7 @@ public class JWTTokenStoreConfiguration {
 
 
     @Bean
+    @Primary
     public DefaultTokenServices tokenServices(TokenStore tokenStore, JwtAccessTokenConverter tokenEnhancer, ClientDetailsService clientDetailsService) {
         DefaultTokenServices tokenServices = new DefaultTokenServices();
         tokenServices.setTokenStore(tokenStore);
--- src/main/java/com/monkeyk/sos/config/JdbcTokenStoreConfiguration.java
+++ src/main/java/com/monkeyk/sos/config/JdbcTokenStoreConfiguration.java
@@ -3,6 +3,7 @@ package com.monkeyk.sos.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -36,6 +37,7 @@ public class JdbcTokenStoreConfiguration {
 
 
     @Bean
+    @Primary
     public DefaultTokenServices tokenServices(TokenStore tokenStore, ClientDetailsService clientDetailsService) {
         DefaultTokenServices tokenServices = new DefaultTokenServices();
         tokenServices.setTokenStore(tokenStore);
